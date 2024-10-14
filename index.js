2f48d6e8f30b33cde74a3611d6cc505e00209fa2
lorna gathumbi <nyawiralornah@gmail.com>
2024-10-14 10:27:10 +0300

    js fetch

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(books => {
+      renderBooks(books);
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 }
 
 function renderBooks(books) {
